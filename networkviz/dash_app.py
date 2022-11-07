bc5525adea555e7180c5aa543d4284f9c97a4cc1
dbabichenko <dmb72@pitt.edu>
2022-11-07 11:59:34 -0500

    Added 9-11 example, reorganized code

--- networkviz/dash_app.py
+++ networkviz/dash_app.py
@@ -1,7 +1,32 @@
 """
-Author: Mohit Mayank
+Based on Mohit Mayank's tutorial on representing the plot of 
+Game of Thrones network using Visdcc in Dash.
 
-Idea: Plot Game of Thrones network using Visdcc in Dash.
+The original example was modified to account for changes in Dash and 
+Visdcc API, and simplified for the purposes of this class.
+
+#### Installation instructions:
+In order to run this tutorial, the following libraries must be installed:
+* pandas
+* networkx
+* matplotlib
+* pyvis
+* dash
+* visdcc
+Note that installing some of these libraries these libraries on top of your core Python or Anaconda install may cause conflicts and version collission issues.  In order to avoid these issues, I highly recommend doing this within a virtual environment.
+
+#### Virtual environment setup instructions
+1. In your project folder, set up a virtual environment: _python3 -m venv /path/to/new/virtual/environment_
+2. Launch your virtual environment.  For example, if your virtual environment folder is called _env_, you can launch it by executing _source ./env/bin/activate_. You can find a complete tutorial at https://docs.python.org/3/library/venv.html
+3. Install libraries:
+    * ./env/bin/pip3 install pandas
+    * ./env/bin/pip3 install networkx
+    * ./env/bin/pip3 install matplotlib
+    * ./env/bin/pip3 install pyvis
+    * ./env/bin/pip3 install dash
+    * ./env/bin/pip3 install visdcc
+    
+
 """
 # imports
 import dash
